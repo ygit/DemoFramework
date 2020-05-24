220dfd7e1089a3c67b3fbee743c70acc640da220
Yogesh Singh <yogesh.singh.2810@gmail.com>
2020-05-24 23:46:05 +0530

    Initial Commit

---
+++ DemoFramework/DemoFramework.h
@@ -0,0 +1,19 @@
+//
+//  DemoFramework.h
+//  DemoFramework
+//
+//  Created by Yogesh Singh on 24/05/20.
+//  Copyright © 2020 Yogesh Singh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DemoFramework.
+FOUNDATION_EXPORT double DemoFrameworkVersionNumber;
+
+//! Project version string for DemoFramework.
+FOUNDATION_EXPORT const unsigned char DemoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoFramework/PublicHeader.h>
+
+
